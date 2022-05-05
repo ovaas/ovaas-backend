109ba726bf67d6d3a9db72841c9adf698941d173
Yusuke Sakabe <58514643+yusuke-1105@users.noreply.github.com>
2022-05-05 16:51:03 +0900

    pref: speeded up make_human_mask function (#10)
    Co-authored-by: Han（ハン） <me@hanlee.co>

--- hidedistantperson/postprocessor.py
+++ hidedistantperson/postprocessor.py
@@ -1,35 +1,22 @@
 import cv2
-import itertools
 import numpy as np
 from PIL import Image
 
-CLASSES_COLOR_MAP = [ (255, 255, 255),(255, 255, 255),(255, 255, 255),(255, 255, 255),
-                      (255, 255, 255),(255, 255, 255),(255, 255, 255),(255, 255, 255),
-                      (255, 255, 255),(255, 255, 255),(255, 255, 255),(0, 0, 0),
-                      (255, 255, 255),(255, 255, 255),(255, 255, 255),(255, 255, 255),
-                      (255, 255, 255),(255, 255, 255),(255, 255, 255),(255, 255, 255) ]
-
-# make a segmentation mask image
+# make segmentation mask image
 def make_human_mask(res: list, w: int, h: int) -> Image:
 
     if len(res.shape) == 3: res = np.expand_dims(res, axis=1)
 
-    *_, out_h, out_w, data = *res.shape, res[0]
-
-    # make a mask image cropped in the shape of a person.
-    CLASSES_MAP = np.array([CLASSES_COLOR_MAP[min((int(data[:, i, j])\
-                            if len(data[:, i, j]) == 1 else np.argmax(data[:, i, j])), 20)] \
-                            for i, j in itertools.product(range(out_h), range(out_w))]).reshape(out_h, out_w, 3)
+    #　make a mask image cropped in the shape of a person.
+    data = res[0].transpose((1, 2, 0))
+    data = np.where(data == 11, (0, 0, 0), (255, 255, 255))     # 11: person
 
     # convert to gray scale
-    mask = CLASSES_MAP.astype(np.uint8)
-    mask = cv2.cvtColor(mask, cv2.COLOR_BGR2GRAY)
+    mask = Image.fromarray(data.astype(np.uint8)).convert('L')
 
-    mask = cv2.resize(mask, (w, h))
-    
-    return Image.fromarray(mask)
+    return mask.resize((w, h))
 
-# make a depth mask
+# make segmentation mask
 def make_mono_mask(output, w: int, h: int) -> Image:
 
     mask = cv2.resize(output, (w, h))
@@ -37,10 +24,10 @@ def make_mono_mask(output, w: int, h: int) -> Image:
     # scaling
     mask_min, mask_max = mask.min(), mask.max()
 
-    if mask_max - mask_min > 1e-6: mask=(mask-mask_min)/(mask_max-mask_min)
+    if mask_max - mask_min > 1e-6: mask = (mask - mask_min) / (mask_max - mask_min)
     else: mask.fill(0.5)
 
     # convert to gray scale
-    mask=(255-mask*255).astype(np.uint8)
-    
+    mask = (255 - mask * 255).astype(np.uint8)
+
     return Image.fromarray(mask)
--- humansegmentation/postprocessor.py
+++ humansegmentation/postprocessor.py
@@ -1,26 +1,16 @@
-import itertools
 import numpy as np
 from PIL import Image
 
-CLASSES_COLOR_MAP = [ (255, 255, 255),(255, 255, 255),(255, 255, 255),(255, 255, 255),
-                      (255, 255, 255),(255, 255, 255),(255, 255, 255),(255, 255, 255),
-                      (255, 255, 255),(255, 255, 255),(255, 255, 255),(0, 0, 0),
-                      (255, 255, 255),(255, 255, 255),(255, 255, 255),(255, 255, 255),
-                      (255, 255, 255),(255, 255, 255),(255, 255, 255),(255, 255, 255) ]
-
 # make segmentation mask image
-def segmentation(res: list, w: int, h: int) -> Image:
+def make_human_mask(res: list, w: int, h: int) -> Image:
 
     if len(res.shape) == 3: res = np.expand_dims(res, axis=1)
 
-    *_, out_h, out_w, data = *res.shape, res[0]
-
-    # make a mask image cropped in the shape of a person.
-    CLASSES_MAP = np.array([CLASSES_COLOR_MAP[min((int(data[:, i, j])\
-                            if len(data[:, i, j]) == 1 else np.argmax(data[:, i, j])), 20)] \
-                            for i, j in itertools.product(range(out_h), range(out_w))]).reshape(out_h, out_w, 3)
+    #　make a mask image cropped in the shape of a person.
+    data = res[0].transpose((1, 2, 0))
+    data = np.where(data == 11, (0, 0, 0), (255, 255, 255))     # 11: person
 
     # convert to gray scale
-    mask_img = Image.fromarray(CLASSES_MAP.astype(np.uint8)).convert('L')
+    mask = Image.fromarray(data.astype(np.uint8)).convert('L')
     
-    return mask_img.resize((w, h))
+    return mask.resize((w, h))
